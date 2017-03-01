a84d8414cb3df5fbe6477e97f6aa4d430d69c53d
Mehmet Çağatay Barın <mcagataybarin@gmail.com>
2017-03-01 12:56:37 +0300

    Font added

--- AndroQuiz/app/src/main/java/com/example/mcagataybarin/androquiz/MainActivity.java
+++ AndroQuiz/app/src/main/java/com/example/mcagataybarin/androquiz/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.mcagataybarin.androquiz;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView name = (TextView) findViewById(R.id.textView2);
+        Typeface face = Typeface.createFromAsset(getAssets(),
+                "cartoon_font.ttf");
+        name.setTypeface(face);
     }
 
     public void usernameEntered(View view) {
